c9cc361e1f3fb704f5c5144d56f8ad713c5a2b0c
Andrew McDougal <andrew@ontrendwebsites.com>
2019-04-14 08:24:05 +1000

    Add footer and header.
    Turn into a child theme using bones as parent.

--- functions.php
+++ functions.php
@@ -11,13 +11,13 @@ sidebars, comments, etc.
 
 // ONTREND FUNCTIONS
 
-function prefix_add_footer_styles() {
-  wp_enqueue_style('googleFonts', '//fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic|Cormorant+Garamond:400,400i,700');
-  wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.5.0/css/all.css');
-  wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/library/js/ontrend.js?v=1' );
-  wp_enqueue_style( 'hamburger-css', get_stylesheet_directory_uri() . '/library/css/hamburgers/hamburgers.css' );
-  wp_enqueue_style( 'ontrend-css', get_stylesheet_directory_uri() . '/library/css/ontrend.css?v=3' );
-};
-add_action( 'get_footer', 'prefix_add_footer_styles' );
+
+
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+}
 
 /* DON'T DELETE THIS CLOSING TAG */ ?>
