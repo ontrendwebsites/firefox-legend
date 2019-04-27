48d0efc1693ce482a426453557a42cb7dca69c1c
Andrew McDougal <andrew@ontrendwebsites.com>
2019-04-27 11:01:53 +1000

    Add header file to style header
    Seaprate scripts to footer, css to head.
    Add normalize css

--- functions.php
+++ functions.php
@@ -16,16 +16,16 @@ sidebars, comments, etc.
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
+  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', '', false );
+  wp_enqueue_style( 'normalize-css', get_stylesheet_directory_uri() . '/library/css/normalize.css', '', false );
+  wp_enqueue_style( 'hamburger-css', get_stylesheet_directory_uri() . '/library/css/hamburgers.css', '', false );
+  wp_enqueue_style( 'ontrend-css', get_stylesheet_directory_uri() . '/library/css/ontrend.css?v=3', '', false ); // false adds it to head
 }
 
 // ONTREND FUNCTIONS
 
 function add_footer_styles_and_scripts() {
-  wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/library/js/ontrend.js?v=1' );
-  wp_enqueue_style( 'hamburger-css', get_stylesheet_directory_uri() . '/library/css/hamburgers.css' );
-  wp_enqueue_style( 'ontrend-css', get_stylesheet_directory_uri() . '/library/css/ontrend.css?v=3' );
+  wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/library/js/ontrend.js?v=1', '', false ); // true adds it to just before closing body tag
 };
 add_action( 'get_footer', 'add_footer_styles_and_scripts' );
 
